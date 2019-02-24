6ca6e67dd2fad644c126f3a4e3bb4cb4462e5084
ttam184 <39779837+ttam184@users.noreply.github.com>
2019-02-24 18:32:03 -0500

    Add files via upload

---
+++ SOS/SOS.ino
@@ -0,0 +1,49 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(11,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(11,HIGH);
+  delay(200);
+  digitalWrite(11,LOW);
+  delay(200);
+  digitalWrite(11,HIGH);
+  delay(200);
+  digitalWrite(11,LOW);
+  delay(200);
+  digitalWrite(11,HIGH);
+  delay(200);
+  digitalWrite(11,LOW);
+  delay(500);
+  
+  digitalWrite(11,HIGH);
+  delay(1000);
+  digitalWrite(11,LOW);
+  delay(200);
+  digitalWrite(11,HIGH);
+  delay(1000);
+  digitalWrite(11,LOW);
+  delay(200);
+  digitalWrite(11,HIGH);
+  delay(1000);
+  digitalWrite(11,LOW);
+  delay(500);
+
+  digitalWrite(11,HIGH);
+  delay(200);
+  digitalWrite(11,LOW);
+  delay(200);
+  digitalWrite(11,HIGH);
+  delay(200);
+  digitalWrite(11,LOW);
+  delay(200);
+  digitalWrite(11,HIGH);
+  delay(200);
+  digitalWrite(11,LOW);
+  delay(200);
+
+  delay(1000);
+}
